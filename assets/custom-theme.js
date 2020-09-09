3b10284922705eb9413a66d20d33befce2230115
Kassio Schaider <kassio.schaider@hotmail.com>
2020-09-09 14:41:30 -0300

    Adcionando function para remover header bars.

--- assets/custom-theme.js
+++ assets/custom-theme.js
@@ -3,3 +3,12 @@ $('.accordion').on('click', function(event) {
     event.preventDefault();
     $(this).toggleClass('active').siblings().toggle();
 });
+
+window.onscroll = function() {topBar()};
+
+function topBar() {
+
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        $("#header-bars").remove();
+    }
+}
