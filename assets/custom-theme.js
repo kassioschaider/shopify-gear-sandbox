40571507efc22db03f316336acdf0ad54577a1fc
Kassio Schaider <kassio.schaider@hotmail.com>
2020-09-09 15:07:46 -0300

    Add header-onroll para alterar o header ao rolar o mouse.

--- assets/custom-theme.js
+++ assets/custom-theme.js
@@ -10,5 +10,6 @@ function topBar() {
 
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         $("#header-bars").remove();
+        $('.site-header').addClass('header-onroll');
     }
 }
